46b33e8916801848bc7afbb4d1f34c77d9ddc0fe
nameisbri <gcdbarreira@gmail.com>
2025-02-17 21:22:28 -0500

    create initial share page layout

--- src/components/ActionButtons/ActionButtons.jsx
+++ src/components/ActionButtons/ActionButtons.jsx
@@ -1,10 +1,16 @@
 import "./ActionButtons.scss";
 import { Upload, Calendar } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const ActionButtons = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="action-buttons">
-      <button className="action-buttons__button">
+      <button
+        className="action-buttons__button"
+        onClick={() => navigate("/upload")}
+      >
         <Upload className="action-buttons__icon" />
         <span className="action-buttons__text">Upload Results</span>
       </button>
