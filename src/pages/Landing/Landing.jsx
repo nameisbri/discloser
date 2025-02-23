80f85da6619de09a9f276ca34997e3b9e43ac599
nameisbri <gcdbarreira@gmail.com>
2025-02-23 18:59:19 -0500

    add landing page and conditional navigation

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { TestTubeDiagonal, Heart, ShieldCheck } from "lucide-react";
 import "./Landing.scss";
 import { useNavigate } from "react-router-dom";
+import logo from "../../assets/emblem_discloser_darkpink.svg";
 
 const Landing = () => {
   const [activeFeature, setActiveFeature] = useState(null);
@@ -44,23 +45,28 @@ const Landing = () => {
     <div className="landing">
       <div className="landing__hero">
         <div className="landing__hero-content">
+          <img className="landing__hero-logo" src={logo} alt="discloser logo" />
           <h1 className="landing__title">
-            Own Your{" "}
-            <span className="landing__title-highlight">Sexual Health</span>
+            <span className="landing__title-highlight">discloser</span>
           </h1>
           <p className="landing__subtitle">
-            Empowering you to track, share, and understand your sexual wellness
-            with confidence and privacy.
+            Share sexual health information mindfully
+          </p>
+          <p className="landing__subtitle">
+            Take control of your sexual health journey with a secure, discreet
+            way to manage and share your test results. Focus on what matters -
+            having informed, confident conversations about sexual health.
+          </p>
+          <p className="landing__subtitle">
+            Focus on what matters - having informed, confident conversations
+            about sexual health.
           </p>
           <div className="landing__cta-group">
             <button
               className="landing__cta-button landing__cta-button--primary"
               onClick={handleQuickStart}
             >
-              Quick Start
-            </button>
-            <button className="landing__cta-button landing__cta-button--secondary">
-              Learn More
+              Get started
             </button>
           </div>
         </div>
